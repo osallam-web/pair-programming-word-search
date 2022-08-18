575e4e2b7b674a597779b75a93b24f1da8244cb3
Omar <omar.sallam@gmail.com>
2022-08-17 22:33:47 -0400

    pair programming exercise for Catherine and Omar

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
     }
+
+    for (const l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
 }
+
+function transpose(matrix) {
+    let result = new Array(matrix[0].length);
+
+    for (let i = 0; i < result.length; i++) {
+        result[i] = new Array(matrix.length);
+        for (let j = 0; j < matrix.length; j++) {
+            result[i][j] = matrix[j][i];
+        }
+    }
+    return result;
+}
+
 
 module.exports = wordSearch
